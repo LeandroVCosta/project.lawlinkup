3b46f90b37541e426da86d5e493e765496f392c3
LeandroVCosta <leandrocosta@sptech.school>
2023-03-05 00:55:17 -0300

    Alterando estrutura de pastas

--- src/loginlogoff/src/main/kotlin/lawlinkup/loginlogoff/Conta.kt
+++
@@ -1,9 +0,0 @@
-package lawlinkup.loginlogoff
-
-data class Conta(
-    val nome:String = "",
-    val senha:String = "",
-    val email:String = "",
-    val tipoUsuario:Int = 0,
-    var logado:Boolean = false
-)
